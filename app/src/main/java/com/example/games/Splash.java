b0a340a42639101fe70385cced9be614cc8f786f
Rama <ramago1715@gmail.com>
2024-02-21 04:53:30 +0100

    splash and rewind 1 movement

---
+++ app/src/main/java/com/example/games/Splash.java
@@ -0,0 +1,44 @@
+package com.example.games;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    private static final int SPLASH_TIME_OUT = 1000;
+    private ImageView imageView;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        imageView = findViewById(R.id.cookieicon);
+        textView = findViewById(R.id.cookietext);
+
+
+        Animation slideUpAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_up);
+        imageView.startAnimation(slideUpAnimation);
+
+
+        Animation slideUpAnimationForText = AnimationUtils.loadAnimation(this, R.anim.slide_up);
+        textView.startAnimation(slideUpAnimationForText);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
